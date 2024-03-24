57a23ae1a6ee6b8e97e2721756bfde5ac478e09e
Daniela Leal <falfada@gmail.com>
2024-03-24 11:58:55 +1100

    Added Dark Mode Functionality

---
+++ assets/js/logic.js
@@ -0,0 +1,17 @@
+// Dark Mode Functionality
+const themeToggle = document.getElementById('darkmode-toggle');
+const bodyContainer = document.body;
+
+console.log(bodyContainer);
+
+let mode = "light";
+
+themeToggle.addEventListener("click", function(){
+    if(mode === "light"){
+        mode = "dark";
+        bodyContainer.setAttribute("class", "dark");
+    } else{
+        mode = "light";
+        bodyContainer.setAttribute("class", "light");
+    }
+});
