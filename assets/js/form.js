f87d6387ebddc568b7cd22cb07b3f836a0b7fb07
Daniela Leal <falfada@gmail.com>
2024-03-26 20:04:53 +1100

    Added functionality to validate that none of the input values are empty

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,31 +1,47 @@
-// When Form submits:
-// validates all input fields that are not empty
-// if empty: show error message
-// store data in localStorage [solved]
-// redirect to blog.html  [solved]
 const usernameInput = document.querySelector("#blogUsername");
 const blogTitle = document.querySelector("#blogTitle");
 const blogContent = document.querySelector("#blogContent");
 const blogSubmit = document.querySelector("#blogSubmit");
+const errorMessage = document.querySelector("#errorMessage");
 
 blogSubmit.addEventListener("click", storeBlogInfo);
 
+//Function to show error message
+function displayMessage(type, message) {
+  errorMessage.textContent = message;
+  errorMessage.setAttribute("class", type);
+}
+
+//Function to store data in localStorage
 function storeBlogInfo(event) {
   event.preventDefault();
-  // Storing Information in Local Storage
-  let posts = [];
 
-  if (localStorage.getItem("blogInfo") !== null) {
-    posts = JSON.parse(localStorage.getItem("blogInfo"));
+  //Validating if input values are empty
+  const userValue = usernameInput.value;
+  const titleValue = blogTitle.value;
+  const contentValue = blogContent.value;
+  if (userValue === "") {
+    displayMessage("error", "Email cannot be blank");
+  } else if (titleValue === "") {
+    displayMessage("error", "Title cannot be blank");
+  } else if (contentValue === "") {
+    displayMessage("error", "Content cannot be blank");
+  } else {
+    // Storing Information in Local Storage
+    let posts = [];
+
+    if (localStorage.getItem("blogInfo") !== null) {
+      posts = JSON.parse(localStorage.getItem("blogInfo"));
+    }
+    const blogInfo = {
+      user: userValue,
+      title: titleValue,
+      content: contentValue,
+    };
+
+    posts.push(blogInfo);
+
+    localStorage.setItem("blogInfo", JSON.stringify(posts));
+    window.location.href = "blog.html";
   }
-  const blogInfo = {
-    user: usernameInput.value,
-    title: blogTitle.value,
-    content: blogContent.value,
-  };
-
-  posts.push(blogInfo);
-
-  localStorage.setItem("blogInfo", JSON.stringify(posts));
-  window.location.href = "blog.html";
 }
